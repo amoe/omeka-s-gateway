01d8653fae9825294a281824cf276efae2608f2c
David Banks <amoebae@gmail.com>
2020-12-02 16:44:34 +0000

    prepping for pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="omeka-s-gateway",
+    version="0.0.1",
+    author="David Banks",
+    author_email="amoebae@gmail.com",
+    description="Omeka-S REST API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amoe/omeka-s-gateway",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
